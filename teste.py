8b4abfda45cc292bf76ba89ad9955547dc78f261
Rennan Pontes Cardoso <rennan.cardoso@FOCO.LOCAL>
2026-03-04 17:06:49 -0300

    primeiro commit

---
+++ teste.py
@@ -0,0 +1,19 @@
+import requests
+import time
+
+HEALTHCHECK_URL = "https://hc-ping.com/70f89754-ef69-476e-aa0e-595b67765cf6"
+
+def main():
+    print("Executando rotina...")
+    time.sleep(2)
+
+    # Simulação de sucesso
+    print("Rotina finalizada com sucesso")
+
+    # Envia ping de sucesso
+    requests.get(HEALTHCHECK_URL)
+
+
+
+if __name__ == "__main__":
+    main()

47377f9d9078a3f34b503fc49a0dedbc48247c31
Rennan Pontes Cardoso <rennan.cardoso@FOCO.LOCAL>
2026-03-09 07:52:46 -0300

    atualizacao dos textos

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ import time
 HEALTHCHECK_URL = "https://hc-ping.com/70f89754-ef69-476e-aa0e-595b67765cf6"
 
 def main():
-    print("Executando rotina...")
+    print("Executando rotina de teste...")
     time.sleep(2)
 
     # Simulação de sucesso
@@ -12,7 +12,7 @@ def main():
 
     # Envia ping de sucesso
     requests.get(HEALTHCHECK_URL)
-
+    print("Ping realizado com sucesso")
 
 
 if __name__ == "__main__":
